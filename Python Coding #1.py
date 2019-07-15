34cb6f8e57ec79e63e3170dcce91cfea7c5e0b89
Wildfire4496 <52932163+Wildfire4496@users.noreply.github.com>
2019-07-15 15:56:54 -0400

    Add files via upload
    1st day of Python coding with STEM

---
+++ Python Coding #1.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 11:28:02 2019
+
+@author: admin
+"""
+
+pwd
+
+5/8 #division
+
+print ("hello world")
+
+print ("I am learning to code in python today")
+
+print(53)
+
+53
+
+"hello world"
+
+print (5/2)
+
+print (5-2)
+
+print (5^2)
+
+print (8/9*3)
+
+print (5*3)
+
+print("the division of 5/2 is", 5/2)
+
+#create function to sum any two numbers
+    
+    def plus(a,b,c):
+        plus= a + b + c
+        print(plus)
+        plus(2,4,7)
+
+        #create function to calculate area of triangle
+        def multiply (a,b,c):
+            multiply= (a*b*c)
+            print(multiply)
+            plus(1/2, 6, 4)
