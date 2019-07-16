563b02487c77f788c9188603be9f15836df75ea9
Wildfire4496 <52932163+Wildfire4496@users.noreply.github.com>
2019-07-16 16:51:06 -0400

    Add files via upload

---
+++ Python day 2!!! (some of it i lost the rest).py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 15:43:30 2019
+
+@author: admin
+"""
+
+def cubicroot(x):
+    cubicroot=x**1/3
+    print(cubicroot)
+    
+    cubicroot(27)
+
+
+def cubicroot(x):
+    cubicroot=x**1/3
+    print(cubicroot)
+    cubicroot(57)
+
+cube= "please enter value to the variable t"
+cubicroot= math.pow(t,1/3)    
+cubicroot(t)
+cubicroot(44)
+
